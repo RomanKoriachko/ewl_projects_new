af7f0c7d2e8782e0be9ff734b04610f9f413d26f
Roman <mazaxaka.tyt@gmail.com>
2022-10-31 18:05:40 +0200

    delite AddNewproject

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -1,6 +1,5 @@
 import './Main.scss'
-import { getDatabase, ref, onValue } from 'firebase/database'
-import AddNewProject from 'components/mainComponents/AddNewProject/AddNewProject'
+import { getDatabase, ref, onValue, set } from 'firebase/database'
 import { useState } from 'react'
 import RegistrationAndLogin from 'components/mainComponents/RegistrationAndLogin/RegistrationAndLogin'
 
@@ -11,6 +10,12 @@ export type UserType = {
     password: string
     hasAccount: boolean
     isAdmin: boolean
+}
+
+type ProjectType = {
+    country: string
+    salary: string
+    projectName: string
 }
 
 const Main = (props: Props) => {
@@ -27,10 +32,85 @@ const Main = (props: Props) => {
         isAdmin: false,
     })
 
-    // ----------------------------- read data -----------------------------
+    // ----------------------------- write data --------------------------------
+
+    const [project, setNewProject] = useState<ProjectType>({
+        country: '',
+        salary: '',
+        projectName: '',
+    })
+
+    const handleChangeCountry = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewProject((prevState: ProjectType) => ({
+            ...prevState,
+            country: e.target.value,
+        }))
+    }
+    const handleChangeSalary = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewProject((prevState: ProjectType) => ({
+            ...prevState,
+            salary: e.target.value,
+        }))
+    }
+    const handleChangeProjectName = (
+        e: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setNewProject((prevState: ProjectType) => ({
+            ...prevState,
+            projectName: e.target.value,
+        }))
+    }
 
     const db = getDatabase()
-    const starCountRef = ref(db, 'vacancy/')
+    const starCountRef = ref(db, `vacancy/`)
+    const dbRef = ref(getDatabase())
+
+    function writeProjectData(
+        country: string,
+        salary: string,
+        projectName: string
+    ) {
+        onValue(starCountRef, (snapshot) => {
+            const data = snapshot.val()
+            console.log(data)
+            if (data.hasOwnProperty(project.projectName)) {
+                setNewProject(() => ({
+                    country: '',
+                    salary: '',
+                    projectName: '',
+                }))
+            } else {
+                set(ref(db, `vacancy/${projectName}/`), {
+                    country: country,
+                    salary: salary,
+                })
+            }
+        })
+    }
+
+    const onSendClick = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (
+            project.country === '' ||
+            project.salary === '' ||
+            project.projectName === ''
+        ) {
+            alert("всі поля обов'язкові")
+        } else {
+            writeProjectData(
+                project.country,
+                project.salary,
+                project.projectName
+            )
+            setNewProject(() => ({
+                country: '',
+                salary: '',
+                projectName: '',
+            }))
+        }
+    }
+
+    // ----------------------------- read data -----------------------------
 
     let newDataArr
     let newData = {}
@@ -49,7 +129,33 @@ const Main = (props: Props) => {
         <div>
             {loginData.isAdmin ? (
                 <div>
-                    <AddNewProject />
+                    <div className="add-project">
+                        <p>Додати проект</p>
+                        <form onSubmit={onSendClick}>
+                            <input
+                                type="text"
+                                id="country"
+                                placeholder="Назва країни"
+                                value={project.country}
+                                onChange={handleChangeCountry}
+                            />
+                            <input
+                                type="text"
+                                id="salary"
+                                placeholder="Ставка"
+                                value={project.salary}
+                                onChange={handleChangeSalary}
+                            />
+                            <input
+                                type="text"
+                                id="project"
+                                placeholder="назва проекту"
+                                value={project.projectName}
+                                onChange={handleChangeProjectName}
+                            />
+                            <button type="submit">Додати проект</button>
+                        </form>
+                    </div>
                     <div className="show-projects">
                         <div>
                             {
